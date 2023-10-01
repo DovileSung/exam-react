2dcee5bc932be8e08858fb624592b0a4c5aac65a
Dovile <dovile.sung@gmail.com>
2023-10-01 21:46:32 +0300

    first commit

---
+++ exam/src/App.jsx
@@ -0,0 +1,38 @@
+import logo from "./logo.svg";
+import "./App.css";
+import RegistrationForm from "./components/RegistrationForm";
+import ImageComponent from "./components/ImageComponents";
+import Modal from "./components/RegistrationForm";
+import React, { useState } from "react";
+
+function App() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
+
+  return (
+    <div>
+      <button onClick={openModal}>Sign Up TEST</button>
+      {isModalOpen && (
+        <Modal closeModal={closeModal}>
+          <div className="registration-page">
+            <div className="left-content">
+              <imageComponent />
+            </div>
+            <div className="right-content">
+              <RegistrationForm />
+            </div>
+          </div>
+        </Modal>
+      )}
+    </div>
+  );
+}
+
+export default App;
---
+++ exam/src/components/ImageComponents.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function image() {
+  return (
+    <div>
+      <h1> LAPTOP WORLD</h1>
+      <img src="Laptop.jpg" alt="Image" className="image" />
+      <img src="Facebook_Logo.png" alt="Logo" className="logo" />
+    </div>
+  );
+}
+export default image;
